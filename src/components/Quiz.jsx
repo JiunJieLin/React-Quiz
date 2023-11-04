44ff1ced15df97fea5ca49c3108ca414deebdddb
JiunJieLin <sl10066116@gmail.com>
2023-11-04 18:50:29 +0800

    collect user's answer

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+import dummyQuestions from "../questions";
+
 const Quiz = () => {
-  return;
+  const [userAnswers, setUserAnswers] = useState([]);
+  const activeQuestionIndex = userAnswers.length;
+  const handleSelectAnswer = (selectedAnswer) => {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+    console.log(userAnswers);
+  };
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{dummyQuestions[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {dummyQuestions[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Quiz;
